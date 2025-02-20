926993038a7e6e1501093dafef9fc3b77aa1b8d2
Cameron <48329714+CNimmo16@users.noreply.github.com>
2025-02-20 13:35:17 +0000

    use chunking for download

--- src/bin/load_data.py
+++ src/bin/load_data.py
@@ -6,6 +6,7 @@ import aiohttp
 import asyncio
 import os
 from tqdm import tqdm
+import numpy as np
 
 from util import mini
 
@@ -13,7 +14,49 @@ dirname = os.path.dirname(__file__)
 
 NUM_IMAGES = 1000 if mini.is_mini() else 100_000
 
+CHUNK_SIZE = 10_000
+
 def main():
+    csv_out_path = os.path.join(dirname, '../../data/recipes.csv')
+    img_out_dir = os.path.join(dirname, '../../data/dataset-b-images')
+
+    if os.path.exists(csv_out_path):
+        raise Exception(f"Path already exists {csv_out_path}")
+
+    if os.path.exists(img_out_dir):
+        raise Exception(f"Path already exists {img_out_dir}")
+    
+    async def download_images(chunk):
+
+        async def process_row(session, row, img_out_dir, pbar):
+            images = row['Images']
+            recipe_id = row['RecipeId']
+
+            if len(images) == 0:
+                df.drop(index=row.name, inplace=True)
+                pbar.update(1)
+                return
+
+            img_url = images[0]
+
+            try:
+                save_path = os.path.join(img_out_dir, f"{recipe_id}.jpg")
+                os.makedirs(os.path.dirname(save_path), exist_ok=True)
+                async with session.get(img_url) as response:
+                    with open(save_path, 'wb') as file:
+                        file.write(await response.read())
+
+            except Exception as e:
+                print(f"Failed to download the image for {recipe_id}: {e}")
+                df.drop(index=row.name, inplace=True)
+            finally:
+                pbar.update(1)
+
+        async with aiohttp.ClientSession() as session:
+            with tqdm(total=len(chunk), desc="Downloading images", unit="file") as pbar:
+                tasks = [process_row(session, row, img_out_dir, pbar) for index, row in chunk.iterrows()]
+                await asyncio.gather(*tasks)
+
     data_path = kagglehub.dataset_download("irkaal/foodcom-recipes-and-reviews")
 
     recipes_path = os.path.join(data_path, 'recipes.parquet')
@@ -24,46 +67,12 @@ def main():
 
     df.dropna(subset=['Images'], inplace=True)
 
-    asyncio.run(download_images(df))
+    chunks = [df.iloc[i:i + CHUNK_SIZE] for i in range(0, len(df), CHUNK_SIZE)]
 
-async def download_images(df):
-    csv_out_path = os.path.join(dirname, '../../data/recipes.csv')
-    img_out_dir = os.path.join(dirname, '../../data/dataset-b-images')
+    for idx, chunk in enumerate(chunks):
+        print(f"Downloading images for chunk {idx + 1} of {len(chunks)}...")
 
-    if os.path.exists(csv_out_path):
-        raise Exception(f"Path already exists {csv_out_path}")
-
-    if os.path.exists(img_out_dir):
-        raise Exception(f"Path already exists {img_out_dir}")
-
-    async def process_row(session, row, img_out_dir, pbar):
-        images = row['Images']
-        recipe_id = row['RecipeId']
-
-        if len(images) == 0:
-            df.drop(index=row.name, inplace=True)
-            pbar.update(1)
-            return
-
-        img_url = images[0]
-
-        try:
-            save_path = os.path.join(img_out_dir, f"{recipe_id}.jpg")
-            os.makedirs(os.path.dirname(save_path), exist_ok=True)
-            async with session.get(img_url) as response:
-                with open(save_path, 'wb') as file:
-                    file.write(await response.read())
-
-        except Exception as e:
-            print(f"Failed to download the image for {recipe_id}: {e}")
-            df.drop(index=row.name, inplace=True)
-        finally:
-            pbar.update(1)
-
-    async with aiohttp.ClientSession() as session:
-        with tqdm(total=len(df), desc="Downloading images", unit="file") as pbar:
-            tasks = [process_row(session, row, img_out_dir, pbar) for index, row in df.iterrows()]
-            await asyncio.gather(*tasks)
+        asyncio.run(download_images(chunk))
 
     print('Done processing, writing final csv...')
 
